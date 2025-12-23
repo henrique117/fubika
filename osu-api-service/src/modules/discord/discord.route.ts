af19aab1439eb41462f1ecda135ba4dafc884731
Henrique <hassis.moreira117@gmail.com>
2025-12-23 02:31:58 -0300

    fix: tirando a outenticação das rotas de linkar o discord

--- osu-api-service/src/modules/discord/discord.route.ts
+++ osu-api-service/src/modules/discord/discord.route.ts
@@ -1,10 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { handleCheckDiscordLink, handleCreateDiscordLink } from "./discord.controller";
-import { authenticate } from "../../middlewares/auth.middleware";
 
 const discordRoutes = async (server: FastifyInstance) => {
-    server.addHook('onRequest', authenticate);
-    
     server.post('/createlink', handleCreateDiscordLink);
     server.post('/checklink', handleCheckDiscordLink);
 }

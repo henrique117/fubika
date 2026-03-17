683e7792895f41f0b3718a23aa3661e2709a933d
gbalbuquerque <gui67785@gmail.com>
2026-03-16 21:12:12 -0300

    fix: corrigindo link do profile na navbar

--- services/web/src/components/layout/navbar/index.tsx
+++ services/web/src/components/layout/navbar/index.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
     const userAvatar = signed && user ? `https://a.fubika.com.br/${user.id}` : 'https://a.fubika.com.br/0'
     useEffect(() => {
         let resizeTimer: number;
-        
+
         const handleResize = () => {
             document.body.classList.add('no-transition');
             clearTimeout(resizeTimer);
@@ -90,14 +90,15 @@ const Navbar: React.FC = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
                                 </button>
                             </div>
-
-                            <img
-                                src={userAvatar}
-                                alt={user.name}
-                                className={style.avatarTrigger}
-                                onClick={() => setMenuOpen(!menuOpen)}
-                                onError={(e) => e.currentTarget.src = 'https://a.bpy.local/0'}
-                            />
+                            <Link to={`/profile/${user.id}`}>
+                                <img
+                                    src={userAvatar}
+                                    alt={user.name}
+                                    className={style.avatarTrigger}
+                                    onClick={() => setMenuOpen(!menuOpen)}
+                                    onError={(e) => e.currentTarget.src = 'https://a.bpy.local/0'}
+                                />
+                            </Link>
 
 
                         </div>
